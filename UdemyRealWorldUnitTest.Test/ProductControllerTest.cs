276d52d739a88854650c3218efe6a9976e5f5068
Göksel Karadağ <goksel.karadag34@gmail.com>
2024-11-29 16:52:52 +0300

    Add project files.

---
+++ UdemyRealWorldUnitTest.Test/ProductControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyRealWordUnitTest.Web.Controllers;
+using UdemyRealWordUnitTest.Web.Models;
+using UdemyRealWordUnitTest.Web.Repository;
+using Xunit;
+
+namespace RealWorldUnitTest.Test
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+        private readonly ProductsController _controller;
+        private readonly List<Product> _products;
+
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_mockRepo.Object);
+
+            _products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" },
+                new Product { Id = 2, Name = "Silgi", Price = 100, Stock = 50, Color = "Beyaz" }
+            };
+        }
+
+        [Fact]
+        public async Task Index_ActionExecutes_ReturnViewResult()
+        {
+            var result = await _controller.Index();
+
+            Assert.IsType<ViewResult>(result); // ViewResult dönüp dönmediğini kontrol et
+        }
+
+        [Fact]
+        public async Task Index_ActionExecutes_ReturnProductList()
+        {
+            // Mock setup (Repository'den ürünlerin döndüğünü simüle etme)
+            _mockRepo.Setup(repo => repo.GetAll()).ReturnsAsync(_products);
+
+            var result = await _controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var productList = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Model);
+
+            Assert.Equal<int>(2,productList.Count());
+        }
+    }
+}
---
+++ UdemyRealWorldUnitTest.Web/Migrations/20241129123523_mig_add_new.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UdemyRealWorldUnitTest.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
